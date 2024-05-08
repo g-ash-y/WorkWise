1a5bd0d20533b8405730778592c1f64f01d2b157
karthikava2003 <lbt21it026@gmail.com>
2024-05-08 22:49:47 +0530

    updated pomo

--- script.js
+++ script.js
@@ -2,53 +2,58 @@ const startEl = document.getElementById("start");
 const stopEl = document.getElementById("stop");
 const resetEl = document.getElementById("reset");
 const timerEl = document.getElementById("timer");
-const breakTime=document.getElementById("Break");
+const breakTimeEl = document.getElementById("break");
+
 let interval;
 let timeLeft = 1500; // 25 minutes
 
 function updateTimer() {
   let minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft % 60;
-  let formattedTime = `${minutes.toString().padStart(1, "0")}:${seconds.toString().padStart(1, "0")}`;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   timerEl.innerHTML = formattedTime;
 }
 
 function startTimer() {
-  interval = setInterval(() => {
-    timeLeft--;
-    updateTimer();
-    if (timeLeft === 0) {
-      clearInterval(interval);
-      alert("Time's up!");
-
-   
-    }
-  }, 1000);
-}
-function breakTimefun(){
- 
-      timeLeft = 300; // 5 minutes interval
+  if (!interval) {
+    interval = setInterval(() => {
+      timeLeft--;
       updateTimer();
-      interval = setInterval(() => {
-        timeLeft--;
-        updateTimer();
-        if (timeLeft === 0) {
-          clearInterval(interval);
-          alert("Time's up for the break!");
-          timeLeft = 1500; // Reset to 25 minutes
-          updateTimer();
-        }
-      }, 1000);
-    }
-  
+      if (timeLeft === 0) {
+        clearInterval(interval);
+        alert("Time's up!");
+        interval = null;
+      }
+    }, 1000);
+  }
+}
 
+function breakTimeFunc() {
+  if (!interval) {
+    timeLeft = 300; // 5 minutes
+    updateTimer();
+    interval = setInterval(() => {
+      timeLeft--;
+      updateTimer();
+      if (timeLeft === 0) {
+        clearInterval(interval);
+        alert("Time's up for the break!");
+        timeLeft = 1500; // Reset to 25 minutes
+        updateTimer();
+        interval = null;
+      }
+    }, 1000);
+  }
+}
 
 function stopTimer() {
   clearInterval(interval);
+  interval = null;
 }
 
 function resetTimer() {
   clearInterval(interval);
+  interval = null;
   timeLeft = 1500; // Reset to 25 minutes
   updateTimer();
 }
@@ -56,4 +61,5 @@ function resetTimer() {
 startEl.addEventListener("click", startTimer);
 stopEl.addEventListener("click", stopTimer);
 resetEl.addEventListener("click", resetTimer);
-breakTime.addEventListener("click",breakTimefun);
+breakTimeEl.addEventListener("click", breakTimeFunc);
+
